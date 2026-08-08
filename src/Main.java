e1bcd1d73a868e77e2842acc9fa8bdb6b7476637
acrono2013 <acrono2013@yandex.ru>
2026-08-08 12:23:44 +0400

    добавили цикл с командой для определения "это буква"

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,12 @@ public class Main {
         char [] chars = phrase.toCharArray();
         int left = 0;
         int right = chars.length - 1;
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            } else if (!Character.isLetter(chars[right])) {
+                right--;
+    }
+}
     }
 }
