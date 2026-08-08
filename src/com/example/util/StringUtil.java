d894703efc038d06e6d65617cfd9bed770918ee6
yakup <acrono2013@yandex.ru>
2026-08-08 20:39:53 +0300

    создали новый класс StringUtil с методом reverseLetters

---
+++ src/com/example/util/StringUtil.java
@@ -0,0 +1,23 @@
+package com.example.util;
+
+public class StringUtil {
+    public static String reverseLetters (String phrase) {
+        char [] chars = phrase.toCharArray();
+        int left = 0;
+        int right = chars.length-1;
+        while (left < right) {
+            if (!Character.isLetter(chars[left])) {
+                left++;
+            } else if (!Character.isLetter(chars[right])) {
+                right--;
+            } else {
+                char tmp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+        return new String(chars);
+    }
+}
