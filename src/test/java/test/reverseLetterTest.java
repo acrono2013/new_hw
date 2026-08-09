b9fa71a8905cf32c9156dd84ed96880ca19c7bbd
acrono2013 <acrono2013@yandex.ru>
2026-08-09 21:08:07 +0400

    добавили второй тест на пустую строку

--- src/test/java/test/reverseLetterTest.java
+++ src/test/java/test/reverseLetterTest.java
@@ -6,21 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class reverseLetterTest {
  @Test
     public void happyPath() {
-        String phrase = "J@va the be$t!123";
-        String result =  StringUtil.reverseLetters(phrase);;
+     String phrase = "J@va the be$t!123";
+     String result = StringUtil.reverseLetters(phrase);
+     ;
      assertEquals(
              "t@eb eht av$J!123",
              result,
              "Must be t@eb eht av$J!123"
      );
+     System.out.println(result);
+ }
      @Test
-     public void happyPath() {
-         String phrase = "J@va the be$t!123";
+     public void emptyString() {
+         String phrase = "    ";
          String result =  StringUtil.reverseLetters(phrase);;
          assertEquals(
-                 "t@eb eht av$J!123",
-                 result,
-                 "Must be t@eb eht av$J!123"
+                 "    ",
+                 result
          );
+         System.out.println(result);
     }
 }
